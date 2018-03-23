ab9f1a676e9262aaf8ffb6872a825a1e3c67d6a3
Andy_xin <wuxinIOS@163.com>
2018-03-23 13:59:54 +0800

    Initial Commit

---
+++ WXImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXImage
+//
+//  Created by apple on 2018/3/23.
+//  Copyright © 2018年 zhongYing_wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXImage
+//
+//  Created by apple on 2018/3/23.
+//  Copyright © 2018年 zhongYing_wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
